da3f1d5b93d3744091b9c133bed07b1bb9f80a81
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-01-20 11:49:43 +0500

    useEffect and fetching data complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import {useEffect,useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  let [data ,setdata] = useState([])
+  let [name,setname] = useState("asad")
+  
+  console.log("satate")
+
+ 
+  useEffect(()=>{
+    console.log("run agin ")
+  })  
+   //rUN ON EVERY sTATE  uPDATE
+
+
+  useEffect(()=>{
+  // axios.delete("https://jsonplaceholder.typicode.com/todos",{
+  //   id: 1
+  // })
+  // axios.post("https://newsapi.org/v2/everything?q=tesla&from=$now.year-$now.month-$now.day&sortBy=publishedAt&apiKey=1866ed19591c4d99880992a3ca614497",{
+  //   name: "shhshshsh"
+  // })
+   axios.get("https://newsapi.org/v2/everything?q=tesla&from=$now.year-$now.month-$now.day&sortBy=publishedAt&apiKey=1866ed19591c4d99880992a3ca614497")
+  .then((snap)=>{
+    console.log(snap)
+    const data1 = snap.data["articles"]
+    console.log(data1)
+    var getdata =[]
+    data1.map((v,i)=>{
+      console.log(v)
+      getdata.push(v)
+
+    })
+    
+    console.log(getdata)
+
+    
+   
+  
+  })
+  },[]) 
+  console.log(data)  //component did Mount 
+  
+  useEffect(()=>{
+    console.log("data update")
+  },[name])   
+
+  return(
+    
+    <div>
+      <h1>Hello</h1>
+      {name}
+      <button onClick={()=>setdata(++data)}>Change value</button>
+{data}
+      <button onClick={()=>setname("omer")}>Change value 2</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
